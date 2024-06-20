cfc1713c13ac73cb2eda093518df9782de942713
Ankit Raj <kumarankitraj1478@gmail.com>
2024-06-20 13:49:21 +0530

    bug fix

--- fronted/vite-project/src/pages/AIgenerator.tsx
+++ fronted/vite-project/src/pages/AIgenerator.tsx
@@ -4,12 +4,10 @@ import { BACKEND_URL } from "../config";
 import { Appbar } from "../components/AppBar";
 import { useNavigate } from "react-router-dom";
 
-
 export const GenerativeAi = () => {
     const [title, setTitle] = useState("");
-
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
-
 
     const generateContent = async (prompt: string) => {
         try {
@@ -32,7 +30,6 @@ export const GenerativeAi = () => {
             );
             let generatedText = response.data.candidates[0].content.parts[0].text;
 
-
             generatedText = generatedText
                 .replace(/##+/g, '')
                 .replace(/\*\*/g, '')
@@ -40,9 +37,7 @@ export const GenerativeAi = () => {
                 .replace(/- /g, '')
                 .replace(/\n+/g, '\n');
 
-
             return generatedText;
-
         } catch (error) {
             console.error("Error generating content:", error);
             return "";
@@ -50,7 +45,7 @@ export const GenerativeAi = () => {
     };
 
     const handleSubmit = async () => {
-
+        setLoading(true);
         const generatedContent = await generateContent(title);
         const res = await axios.post(`${BACKEND_URL}/api/v1/blog`, {
             title,
@@ -63,7 +58,7 @@ export const GenerativeAi = () => {
 
         localStorage.setItem("authoId", res.data.user_id);
         localStorage.setItem("postId", res.data.id);
-
+        setLoading(false);
         navigate(`/blog/${res.data.id}`);
     };
 
@@ -81,15 +76,21 @@ export const GenerativeAi = () => {
                         onChange={(e) => setTitle(e.target.value)}
                     />
                 </div>
-                <div className="my-1 ">
+                <div className="my-1">
                     <button
                         type="submit"
                         onClick={handleSubmit}
                         className="mt-0 inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800"
+                        disabled={loading}
                     >
-                        Generate
+                        {loading ? "Generating..." : "Generate"}
                     </button>
                 </div>
+                {loading && (
+                    <div className="flex justify-center items-center">
+                        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
+                    </div>
+                )}
             </div>
         </div>
     );
